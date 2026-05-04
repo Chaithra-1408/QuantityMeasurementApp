df7266e4d16f6d758b2396ae0c9bc832fda555d2
Chaithra-1408 <mc1104@srmist.edu.in>
2026-05-05 02:19:21 +0530

    Base application setup

---
+++ src/QuantityMeasurementApp.java
@@ -0,0 +1,29 @@
+public class QuantityMeasurementApp {
+
+    static class Feet {
+        private final double value;
+
+        Feet(double value) {
+            this.value = value;
+        }
+
+        @Override
+        public boolean equals(Object obj) {
+            if (this == obj) return true;
+            if (obj == null) return false;
+            if (getClass() != obj.getClass()) return false;
+            Feet other = (Feet) obj;
+            return Double.compare(this.value, other.value) == 0;
+        }
+    }
+
+    public static void main(String[] args) {
+        Feet feet1 = new Feet(1.0);
+        Feet feet2 = new Feet(1.0);
+
+        System.out.println("=== Quantity Measurement App ===");
+        System.out.println("Input: 1.0 ft and 1.0 ft");
+        System.out.println("Equal: " + feet1.equals(feet2));
+        System.out.println("================================");
+    }
+}
