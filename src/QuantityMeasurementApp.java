30a160fc5ddd1a965edd096ea5bbe26b993837c8
Chaithra-1408 <mc1104@srmist.edu.in>
2026-05-05 02:27:37 +0530

    UC3: Generic quantity class for DRY principle

--- src/QuantityMeasurementApp.java
+++ src/QuantityMeasurementApp.java
@@ -1,10 +1,31 @@
 public class QuantityMeasurementApp {
 
-    static class Feet {
-        private final double value;
+    enum LengthUnit {
+        FEET(1.0),
+        INCH(1.0 / 12.0);
 
-        Feet(double value) {
+        private final double conversionFactor;
+
+        LengthUnit(double conversionFactor) {
+            this.conversionFactor = conversionFactor;
+        }
+
+        public double getConversionFactor() {
+            return conversionFactor;
+        }
+    }
+
+    static class QuantityLength {
+        private final double value;
+        private final LengthUnit unit;
+
+        QuantityLength(double value, LengthUnit unit) {
             this.value = value;
+            this.unit = unit;
+        }
+
+        private double toBaseUnit() {
+            return value * unit.getConversionFactor();
         }
 
         @Override
@@ -12,55 +33,46 @@ public class QuantityMeasurementApp {
             if (this == obj) return true;
             if (obj == null) return false;
             if (getClass() != obj.getClass()) return false;
-            Feet other = (Feet) obj;
-            return Double.compare(this.value, other.value) == 0;
+            QuantityLength other = (QuantityLength) obj;
+            return Double.compare(
+                    this.toBaseUnit(), other.toBaseUnit()) == 0;
         }
-    }
-
-    static class Inches {
-        private final double value;
-
-        Inches(double value) {
-            this.value = value;
-        }
-
-        @Override
-        public boolean equals(Object obj) {
-            if (this == obj) return true;
-            if (obj == null) return false;
-            if (getClass() != obj.getClass()) return false;
-            Inches other = (Inches) obj;
-            return Double.compare(this.value, other.value) == 0;
-        }
-    }
-
-    static boolean checkFeetEquality(double val1, double val2) {
-        Feet feet1 = new Feet(val1);
-        Feet feet2 = new Feet(val2);
-        return feet1.equals(feet2);
-    }
-
-    static boolean checkInchesEquality(double val1, double val2) {
-        Inches inches1 = new Inches(val1);
-        Inches inches2 = new Inches(val2);
-        return inches1.equals(inches2);
     }
 
     public static void main(String[] args) {
 
         System.out.println("=== Quantity Measurement App ===");
 
-        System.out.println("Input: 1.0 ft and 1.0 ft");
-        System.out.println("Equal: " + checkFeetEquality(1.0, 1.0));
+        QuantityLength oneFeet = new QuantityLength(
+                1.0, LengthUnit.FEET);
+        QuantityLength twelveInches = new QuantityLength(
+                12.0, LengthUnit.INCH);
+        QuantityLength twoFeet = new QuantityLength(
+                2.0, LengthUnit.FEET);
+        QuantityLength oneInch = new QuantityLength(
+                1.0, LengthUnit.INCH);
+        QuantityLength anotherOneInch = new QuantityLength(
+                1.0, LengthUnit.INCH);
 
-        System.out.println("Input: 1.0 ft and 2.0 ft");
-        System.out.println("Equal: " + checkFeetEquality(1.0, 2.0));
+        System.out.println(
+                "Input: 1.0 feet and 12.0 inches");
+        System.out.println(
+                "Equal: " + oneFeet.equals(twelveInches));
 
-        System.out.println("Input: 1.0 inch and 1.0 inch");
-        System.out.println("Equal: " + checkInchesEquality(1.0, 1.0));
+        System.out.println(
+                "Input: 1.0 feet and 2.0 feet");
+        System.out.println(
+                "Equal: " + oneFeet.equals(twoFeet));
 
-        System.out.println("Input: 1.0 inch and 2.0 inch");
-        System.out.println("Equal: " + checkInchesEquality(1.0, 2.0));
+        System.out.println(
+                "Input: 1.0 inch and 1.0 inch");
+        System.out.println(
+                "Equal: " + oneInch.equals(anotherOneInch));
+
+        System.out.println(
+                "Input: 12.0 inches and 1.0 feet");
+        System.out.println(
+                "Equal: " + twelveInches.equals(oneFeet));
 
         System.out.println("================================");
     }
